1568ae8cb9666432e0c028cbbc0de57e074f6d8e
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2021-11-19 12:20:00 +0000

    feat: Capture golden refernce images

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # visdiff
 
-Visual diffs for a collection of web urls
+This tool allows you to generate screenshots in various screen widths for a list of page urls. Having created a baseline set of 'golden' images, subsequent runs will capture current images and compare at pixle level for any differences. Any deltas are flagged as errors.
+
+This is currently a command line tool, but can be converted fairly easily to run unit tests as part of a CI/CD chain.
 
 ## Installation
 
 ```javascript
-npm i -D @playwright/test
-# install supported browsers
-npx playwright install
-
 npm install
+```
+
+## Creating a baseline set of screenshots
+
+Prior to running comparison tests you must create a set of refernce or 'golden' snapshot images for the list of urls. This should be repeated any time the url list changes, or a page is materially changed and needs to be added into the golden set.
+
+The following command will create baseline images in a sub-folder named `screenshots-golden`:
+
+```javascript
+npm run baseline
+```
+
+## Comparing pages
+
+The following command will create current images in a sub-folder named `screenshots` and visually compare them to golden counterparts:
+
+```javascript
+npm run start
 ```
--- src/index.mjs
+++ src/index.mjs
@@ -12,7 +12,7 @@ const makeFileName = (url) =>
   url.replaceAll(/https:\/\//g, "").replaceAll(/\//g, "-");
 
 // Take a page snapshot for a given url and size
-const takeSnapshot = async ({ browser, url, viewport }) => {
+const takeSnapshot = async ({ browser, url, viewport, isGolden }) => {
   const page = await browser.newPage();
 
   await page.setViewportSize({
@@ -20,17 +20,22 @@ const takeSnapshot = async ({ browser, url, viewport }) => {
     height: viewport.height,
   });
   await page.goto(url);
-  const filename = `${makeFileName(url)}-${viewport.width}.png`;
 
-  await page.screenshot({
-    path: `./screenshots/${filename}`,
-    fullPage: true,
-  });
-  log.info(`Created snapshot ${chalk.cyan(filename)}`);
+  const filename = `${makeFileName(url)}-${viewport.width}.png`;
+  const path = `./screenshots${isGolden ? "-golden" : ""}/${filename}`;
+
+  await page.screenshot({ path, fullPage: true });
+  log.info(
+    `Created ${isGolden ? "golden " : ""}snapshot ${chalk.cyan(filename)}`
+  );
 };
 
 (async () => {
-  log.info("Generating snapshots...");
+  // handle args
+  const args = process.argv.slice(2);
+  const isGolden = args?.[0] === "--golden";
+
+  log.info(`Generating ${isGolden ? "golden " : ""}snapshots...`);
 
   const browser = await chromium.launch();
   const page = await browser.newPage();
@@ -39,7 +44,7 @@ const takeSnapshot = async ({ browser, url, viewport }) => {
   for (const url of urlList) {
     // Take viewport snapshots in parallel
     const snapshotPromises = VIEWPORTS.map((viewport) =>
-      takeSnapshot({ browser, url, viewport })
+      takeSnapshot({ browser, url, viewport, isGolden })
     );
 
     await Promise.all(snapshotPromises);
