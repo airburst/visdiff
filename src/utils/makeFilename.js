3426917ed77bf0a1d73c4cd81d8203ed5f47ead1
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2021-12-10 09:40:06 +0000

    fix: Use Node14 version of replace all

--- src/utils/makeFilename.js
+++ src/utils/makeFilename.js
@@ -1,2 +1,14 @@
-export const makeFileName = (url) =>
-  url.replaceAll(/https:\/\//g, "").replaceAll(/\//g, "-");
+const replaceAll = (string, search, replace) => {
+  return string.split(search).join(replace);
+};
+
+// The Node 16 way
+// export const makeFileName = (url) =>
+//   url.replaceAll(/https:\/\//g, "").replaceAll(/\//g, "-");
+
+// Uses Node 14 compliant function to replace all
+export const makeFileName = (url) => {
+  const withoutHttps = replaceAll(url, /https:\/\//g, "");
+
+  return replaceAll(withoutHttps, /\//g, "-");
+};
